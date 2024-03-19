346022a154a99a2d319213e1a5c84052dbca24a4
Iman <iman.osama2000@gmail.com>
2024-03-19 23:25:55 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const announcementRoutes = require('./routes/announcementRoutes');
+
+// Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
+// Routes
+app.use('/api/announcements', announcementRoutes.router);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB...');
+    // Listen to port
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port... ${process.env.PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log('Error connecting to MongoDB...', err);
+  });
+
